ed1fc0c42d2cbf7020c5e991168fa0c9bc79e362
Elham <elhammirzaei.eetoe@gmail.com>
2026-03-08 16:40:02 +0100

    added the balance calculation and cleaned.

--- mortgageCalculator/src/mortgageCalculator/MortgageCalculator.java
+++ mortgageCalculator/src/mortgageCalculator/MortgageCalculator.java
@@ -2,12 +2,6 @@ package mortgageCalculator;
 
 import java.util.Scanner;
 
-// Mortgage =principal[ i(1 + i)^n ] / [ (1 + i)^n – 1 ]
-// M = Monthly Payment (Principal & Interest)
-//principal= Principal Loan Amount (the total borrowed)
-// i = Monthly Interest Rate (Annual Rate / 12)
-// n = Total Number of Payments (Loan Term in Years * 12)
-
 /**
  * TODO here we calculate the mortgage based on Principal (the amount) and
  * annual interest and the period of the mortgage.
@@ -15,69 +9,44 @@ import java.util.Scanner;
  * @author Elham.Mirzaei
  */
 public class MortgageCalculator {
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 
-		int principal = readPrincipal(scanner);
-		double annualInterestRate = readAnnualInterestRate(scanner);
-		int period = readPeriod(scanner);
+		int principal = (int) readNumber("Principal:", 1000, 1000000);
+		double annualInterestRate = readNumber("Annual Interest Rate:", 1, 30);
+		int period = (int) readNumber("Period (Years):", 1, 30);
 
 		double mortgage = calculateMortgage(principal, annualInterestRate, period);
 
-		System.out.printf("Mortgage: $%.2f%n", mortgage);
-
-		scanner.close();
+		printOutput(principal, annualInterestRate, period, mortgage);
 	}
 
-	/**
-	 * @param scanner
-	 * @return principal
-	 */
-	public static int readPrincipal(Scanner scanner) {
-		int principal;
-		while (true) {
-			System.out.print("Principal ($1k - $1M): ");
-			principal = scanner.nextInt();
-			if (principal >= 1000 && principal <= 1000000)
-				break;
-			System.out.println("Enter a number between 1000 and 1000000.");
+	private static void printOutput(int principal, double annualInterestRate, int period, double mortgage) {
+		System.out.printf("MORTGAGE%n");
+		System.out.printf("-----------%n");
+		System.out.printf("Monthly Payments: $%.2f%n%n", mortgage);
+		System.out.printf("PAYMENT SCHEDULE%n");
+		System.out.printf("----------------%n");
+		for (int month = 1; month <= period * 12; month++) {
+			double balance = calculateBalance(principal, annualInterestRate, period, month);
+			System.out.printf("$%.2f%n", balance);
 		}
-		return principal;
 	}
 
-	/**
-	 * @param scanner
-	 * @return annualInterestRate
-	 */
-	public static double readAnnualInterestRate(Scanner scanner) {
-		double annualInterestRate;
+	public static double readNumber(String prompt, double min, double max) {
+		Scanner scanner = new Scanner(System.in);
+		double value;
 		while (true) {
-			System.out.println("Annual Interest Rate: ");
-			annualInterestRate = scanner.nextDouble();
-			if (annualInterestRate <= 30 && annualInterestRate > 0)
+			System.out.print(prompt);
+			value = scanner.nextDouble();
+			if (value >= min && value <= max)
 				break;
-			System.out.println("Please enter a value greater than 0 and less than or equal to 30!");
+			System.out.println("Enter a number between " + min + "and " + max + ".");
 		}
-		return annualInterestRate;
-	}
-
-	/**
-	 * @param scanner
-	 * @return period
-	 */
-	public static int readPeriod(Scanner scanner) {
-		int period;
-		while (true) {
-			System.out.println("Period (Years): ");
-			period = scanner.nextInt();
-			if (period >= 1 && period <= 30)
-				break;
-			System.out.println("Enter a value between 1 and 30!");
-		}
-		return period;
+		return value;
 	}
 
 	/**
@@ -87,11 +56,26 @@ public class MortgageCalculator {
 	 * @return mortgage
 	 */
 	public static double calculateMortgage(int principal, double annualInterestRate, int period) {
-		double i = annualInterestRate / 100 / 12;
-		int n = period * 12;
 
-		double mortgage = principal * (i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
+		double monthlyInterestRate = annualInterestRate / 100 / 12;
+		int numberOfPayments = period * 12;
+
+		double mortgage = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))
+				/ (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
 		return mortgage;
+	}
+
+	public static double calculateBalance(int principal, double annualInterestRate, int period, int paymentsMade) {
+
+		double monthlyInterestRate = annualInterestRate / 100 / 12;
+		int numberOfPayments = period * 12;
+
+		double balance = principal
+				* (Math.pow((1 + monthlyInterestRate), numberOfPayments)
+						- Math.pow((1 + monthlyInterestRate), paymentsMade))
+				/ (Math.pow((1 + monthlyInterestRate), numberOfPayments) - 1);
+
+		return balance;
 	}
 
 }
