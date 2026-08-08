9beaaf44742d6a8adb5f9fa52fd0837ff09cccb0
Elham <elhammirzaei.eetoe@gmail.com>
2026-08-08 19:43:27 +0200

    up to java 2 session 9 studied, abstraction.

--- src/main/java/mortgageCalculator/Main.java
+++ src/main/java/mortgageCalculator/Main.java
@@ -1,13 +1,14 @@
 package mortgageCalculator;
 
 public class Main {
-	public static void main(String[] args) {
-        var textBox1 = new TextBox();
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text.toUpperCase());
 
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text.toUpperCase());
+        public static void main (String[] args){
+                var employee = new Employee();
+                employee.setBaseSalary(50000);
+                employee.getBaseSalary();
+                employee.setHourlyRate(20);
+                employee.getHourlyRate();
+                int wage = employee.calculateWage(10);
+                System.out.println(wage);
+        }
     }
-}
